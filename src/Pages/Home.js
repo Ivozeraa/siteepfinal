6c982f9872fe0e55753dec254cf047b67392a0d1
Ivozeraa <joseivo2008@gmail.com>
2024-10-28 15:58:05 -0300

    alterações

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,14 +3,18 @@ import Imagem2 from '../components/Imgs/LabLinguas.jpeg';
 import Imagem1 from '../components/Imgs/Frente1.jpeg';
 import { Carrossel } from '../components/Carrossel';
 import Titulo from '../components/Titulo';
+import Styles from "../Css/Home.module.css"
  
 export function Home(){
     return(
         <>
-            <div className="App">
+            <div className={Styles.Home}>
                 <div className="imagemPrincipal"></div>
                 <Carrossel imagem3={Imagem3} imagem2={Imagem2} imagem1={Imagem1}/>
-                <Titulo titulo="opa"/>
+                <Titulo titulo="Objetivo"/>
+                <div className={Styles.container}>
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." </p>
+                </div>
             </div>
         </>
     )
